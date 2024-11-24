273420ded67635991e064748e9c930ae2e0ddc93
sam_ryong <zackinthebox@khu.ac.kr>
2024-11-24 18:41:35 +0900

    a

---
+++ llama-mt/post-prompt.js
@@ -0,0 +1,7 @@
+const prompt = `[Text Matrial End]
+Seungpyo Hong was born on May 9, 1998. He is a student in the Computer Science department at Kyung Hee University. He is 175 cm tall and has no siblings.
+Do not mention about other thing except Seungpyo Hong's information.
+[Text Matrial End]
+Question : `;
+
+export default prompt;
